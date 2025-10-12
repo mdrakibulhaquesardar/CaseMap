ed1357f610e17c284ec49d4e90c60ddb1f34f174
Md Rakibul Haque Sardar <rakibullhaques@gmail.com>
2025-10-12 19:17:13 +0000

    in login page remove footer section

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,5 +2,9 @@
 import LoginForm from "@/components/ui/login-form";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <div className="min-h-screen">
+      <LoginForm />
+    </div>
+  );
 }
