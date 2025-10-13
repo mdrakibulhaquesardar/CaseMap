e55c905aa6bf73854f1e167ef69f5eb2faa1f34c
Md Rakibul Haque Sardar <rakibullhaques@gmail.com>
2025-10-13 14:47:26 +0000

    fix this section make it on center (_for element <Pricing>_)

--- src/components/blocks/Pricing.tsx
+++ src/components/blocks/Pricing.tsx
@@ -95,7 +95,7 @@ export function Pricing({
         </span>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl mx-auto">
         {plans.map((plan, index) => (
           <motion.div
             key={index}
