238ff8939fdc8201a633d5a145d847548f19cb44
Md Rakibul Haque Sardar <rakibullhaques@gmail.com>
2025-10-13 14:45:19 +0000

    home page a priceing sectin ta left a cole gece seta thik kore center a

--- src/components/blocks/Pricing.tsx
+++ src/components/blocks/Pricing.tsx
@@ -95,7 +95,7 @@ export function Pricing({
         </span>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-4 justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center">
         {plans.map((plan, index) => (
           <motion.div
             key={index}
