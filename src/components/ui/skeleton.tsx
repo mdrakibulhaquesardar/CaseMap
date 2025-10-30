3fdb92528dd0708445947cba10e8f0c26c19a90d
Md Rakibul Haque Sardar <rakibullhaques@gmail.com>
2025-10-30 15:45:14 +0000

    skalwton file ta chack koro

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -6,11 +6,9 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("relative overflow-hidden rounded-md bg-muted", className)}
+      className={cn("animate-shimmer relative overflow-hidden rounded-md bg-muted", className)}
       {...props}
-    >
-       <div className="absolute inset-0 -translate-x-full animate-[shimmer_1.5s_infinite] bg-gradient-to-r from-transparent via-background/30 to-transparent"></div>
-    </div>
+    />
   )
 }
 
